26186180ab8ca7fd270e8a690b1228ed1b08268d
Pinki Jha <pinkijha1310@gmail.com>
2024-09-11 01:05:38 +0530

    create new redux store for moviesSlice

--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -1,8 +1,10 @@
+import moviesReducer from "./moviesSlice";
 import userReducer from "./userSlice"
 import { configureStore } from "@reduxjs/toolkit";
 const appStore = configureStore({
     reducer:{
         user: userReducer,
+        movies: moviesReducer,
     },
 });
 
